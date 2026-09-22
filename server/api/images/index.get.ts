718e1299aac20111f6bc73a58098131d74eafe7b
lx521603 <32638642+lx521603@users.noreply.github.com>
2026-09-23 02:57:48 +0800

    Update index.get.ts

--- server/api/images/index.get.ts
+++ server/api/images/index.get.ts
@@ -1,13 +1,5 @@
-export default eventHandler(async () => {
-  try {
-    const result = await hubBlob().list({
-      limit: 1000
-    })
-
-    return result
-  } catch (e) {
-    return {
-      error: String(e)
-    }
-  }
+export default eventHandler(async (event) => {
+  return Object.keys(
+    event.context.cloudflare?.env || {}
+  )
 })
