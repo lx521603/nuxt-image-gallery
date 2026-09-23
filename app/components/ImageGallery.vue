c62e1d75c0504e4c35068c246acc947600f96e9d
lx521603 <x@glee.ch>
2026-09-23 08:40:32 +0800

    style: 限制竖图最大高度为 70vh，等比例缩小防止过大，保持横图不变

--- app/components/ImageGallery.vue
+++ app/components/ImageGallery.vue
@@ -116,9 +116,14 @@ async function clearSession () {
               class="absolute top-4 right-4 z-[9999] opacity-0 group-hover:opacity-100"
               @click="deleteFile(image.pathname)" />
             <NuxtLink :to="`/detail/${image.pathname.split('.')[0]}`" @click="active = image.pathname.split('.')[0]">
-              <img v-if="image" width="527" height="430" :src="`/images/${image.pathname}`"
-                :class="{ imageEl: image.pathname.split('.')[0] === active }"
-                class="h-auto w-full rounded-md transition-all duration-200 border-image brightness-[.8] hover:brightness-100 will-change-[filter]"
+              <img 
+               v-if="image" 
+               width="527" 
+               height="430" 
+               :src="`/images/${image.pathname}`"
+               :class="{ imageEl: image.pathname.split('.')[0] === active }"
+               class="w-full h-auto max-h-[70vh] rounded-md transition-all duration-200 border-image brightness-[.8] hover:brightness-100 will-change-[filter]"
+              >
             </NuxtLink>
           </li>
         </ul>
